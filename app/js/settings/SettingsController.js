434f6426f391f0fc7fbfa22c1984504c4df785e4
Mohamed K. KONE <mkabir.kone@gmail.com>
2021-04-29 16:05:41 +0000

    fix

--- app/js/settings/SettingsController.js
+++ app/js/settings/SettingsController.js
@@ -36,6 +36,39 @@ angular.module('SettingsController', []).controller('SettingsController', ['$sco
 
     //loadSettingss();
 
+    function loadAgents() {
+        openmrsRest.getFull("savicsgmao/agent").then(function (response) {
+            $scope.showLoading = false;
+            $scope.agents = response.results;
+        })
+
+        var region1 = {
+            "regionName": "Ma region 1",
+            "regionCode": "MR1"
+        }
+        var region2 = {
+            "regionName": "Ma region 2",
+            "regionCode": "MR2"
+        }
+    }
+
+    $scope.createRegion = function () {
+        var r = Math.floor(Math.random() * 26000) + 1;
+        var region2 = {
+            "fullName": "Agent Random "+r,
+            "phoneNumber": "98762 087 86 "+r,
+            "agentAddress": "Add "+r,
+            "rollNumber": "0",
+            "isExternal": 0,
+            "department": 1,
+            "siteLocation": 1
+        }
+
+        openmrsRest.create("savicsgmao/agent", region2).then(function (response) {
+            console.log(response)
+        })
+    }
+
     function loadSettingss() {
         openmrsRest.getFull("savicsgmao/Settings").then(function (response) {
             $scope.showLoading = false;
