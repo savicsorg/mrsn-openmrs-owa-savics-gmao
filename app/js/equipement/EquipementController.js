6558677538b3766742f7ee4b3196b905a389e808
Anatole ABE <anatoleabe@gmail.com>
2021-05-18 15:09:01 +0100

    As equipments manager I want to see the list of all the equipments

--- app/js/equipement/EquipementController.js
+++ app/js/equipement/EquipementController.js
@@ -1,38 +1,50 @@
 angular.module('EquipementController', []).controller('EquipementController', ['$scope', '$rootScope', '$state', '$stateParams', 'openmrsRest', function ($scope, $rootScope, $state, $stateParams, openmrsRest) {
-    $scope.rootscope = $rootScope;
-    
-    console.log("EquipementController new form ---")
-    $scope.appTitle = "Gestion des equipements";
-    $scope.resource = "savicsgmao/agent";
-    //Breadcrumbs properties
-    $rootScope.links = {};
-    $rootScope.links["Home"] = "";
-    $rootScope.links["Equipements"] = "/equipements";
-    $scope.equipement = {};
-    
-    $scope.appTitle = "Gestion des equipements";
-    $scope.resource = "savicsgmao/agent";
-    $scope.departments = [
-        { department_id: 1, name: 'Department 1'},
-        { department_id: 2, name: 'Department 2'}
-    ];
-    $scope.equipmentTypes = [
-        { equipment_type_id: 1, name: 'Type 1'},
-        { equipment_type_id: 2, name: 'Type 2'}
-    ];
+        $scope.rootscope = $rootScope;
 
-    
-    //TODO replace this by real data comming from openmrsRest
-    $scope.equipements = [
-        {"equipmentId": 1, "serialnumber": "11KH34567", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
-        {"equipmentId": 2, "serialnumber": "XD1276578", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
-        {"equipmentId": 3, "serialnumber": "567HJG878", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
-        {"equipmentId": 4, "serialnumber": "YTU78645G", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
-        {"equipmentId": 5, "serialnumber": "76GFH6VHB", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"}
-    ];
+        console.log("EquipementController new form ---")
+        $scope.appTitle = "Gestion des equipements";
+        $scope.resource = "savicsgmao/agent";
+        //Breadcrumbs properties
+        $rootScope.links = {};
+        $rootScope.links["Home"] = "";
+        $rootScope.links["Equipements"] = "/equipements";
+        $scope.equipement = {};
 
-    $scope.view = function(id){
-        
-    }
-    
-}]);
+        $scope.appTitle = "Gestion des equipements";
+        $scope.resource = "savicsgmao/agent";
+        $scope.departments = [
+            {department_id: 1, name: 'Department 1'},
+            {department_id: 2, name: 'Department 2'}
+        ];
+        $scope.equipmentTypes = [
+            {equipment_type_id: 1, name: 'Type 1'},
+            {equipment_type_id: 2, name: 'Type 2'}
+        ];
+
+
+        //loadSettingss();
+
+        function loadEquipmnts() {
+            openmrsRest.getFull("savicsgmao/equipment").then(function (response) {
+                $scope.showLoading = false;
+                $scope.equipments = response.results;
+                console.log($scope.equipments)
+            });
+        }
+
+        loadEquipmnts();
+//
+//        //TODO replace this by real data comming from openmrsRest
+//        $scope.equipements = [
+//            {"equipmentId": 1, "serialnumber": "11KH34567", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
+//            {"equipmentId": 2, "serialnumber": "XD1276578", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
+//            {"equipmentId": 3, "serialnumber": "567HJG878", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
+//            {"equipmentId": 4, "serialnumber": "YTU78645G", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"},
+//            {"equipmentId": 5, "serialnumber": "76GFH6VHB", "designation": "Toyota Hilux", "type": "Vehicule", "localization": "Bureau MCD", "status": "En service", "lastModified": "2020-03-12"}
+//        ];
+
+        $scope.view = function (id) {
+
+        }
+
+    }]);
