3234fc891cafcfa3b0ecbfdf097dcef9d3b3bd73
Anatole ABE <anatoleabe@gmail.com>
2021-11-29 17:33:57 +0100

    https://savics.atlassian.net/browse/MRSN-403

--- app/js/equipment/EquipmentController.js
+++ app/js/equipment/EquipmentController.js
@@ -1,40 +1,82 @@
-angular.module('EquipmentController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('EquipmentController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) { 
+angular.module('EquipmentController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('EquipmentController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) {
         $scope.rootscope = $rootScope;
 
-        console.log("EquipmentController new form ---")
-        $scope.appTitle = "Gestion des equipements";
-        $scope.resource = "savicsgmao/agent";
+        $scope.appTitle = $translate.instant("Equipment management");
+        $scope.resource = "savicsgmao";
+        $rootScope.links = {"Pharmacy management module": "", "Equipment": "equipments", "Edit": "equipment"};
+        $scope.loading = false;
+        $scope.equipment = {};
+
         //Breadcrumbs properties
         $rootScope.links = {};
         $rootScope.links["Home"] = "";
         $rootScope.links["Equipements"] = "/equipements";
         $scope.equipement = {};
 
-        $scope.appTitle = "Gestion des equipements";
-        $scope.resource = "savicsgmao/agent";
-        $scope.departments = [
-            {department_id: 1, name: 'Department 1'},
-            {department_id: 2, name: 'Department 2'}
-        ];
-        $scope.equipmentTypes = [
-            {equipment_type_id: 1, name: 'Type 1'},
-            {equipment_type_id: 2, name: 'Type 2'}
-        ];
+        $scope.getEquipmentTypes = function () {
+            return openmrsRest.getFull($scope.resource + "/equipmentType").then(function (response) {
+                $scope.equipmentTypes = response.results;
 
-
-        //loadSettingss();
-
-        function loadEquipmnts() {
-            openmrsRest.getFull("savicsgmao/equipment").then(function (response) {
-                $scope.showLoading = false;
-                $scope.equipments = response.results;
+            }, function (e) {
+                $scope.equipmentTypes = [];
             });
+        };
+        $scope.getEquipmentTypes();
+
+        $scope.getDepartments = function () {
+            return openmrsRest.getFull($scope.resource + "/department").then(function (response) {
+                $scope.departments = response.results;
+
+            }, function (e) {
+                $scope.departments = [];
+            });
+        };
+        $scope.getDepartments();
+
+
+        $scope.save = function () {
+            $scope.loading = true;
+            var query = JSON.parse(JSON.stringify($scope.equipment));
+            if ($scope.equipment && $scope.equipment.serialNumber && $scope.equipment.serialNumber.length >= 3 && $scope.equipment.serialNumber.length <= 120 && $scope.equipment.designation && $scope.equipment.designation != "") {
+                if ($scope.equipment.uuid) {    //Edit
+                    openmrsRest.update($scope.resource + "/equipment", query).then(function (response) {
+                        $scope.equipment = response;
+                        //$scope.getData();
+                        //$state.go('home.equipments', {});
+                        toastr.success($translate.instant('Data saved successfully.'), 'Success');
+                        $state.go('home.equipments');
+                        $scope.loading = false;
+                    }, function (e) {
+                        $scope.loading = false;
+                        toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+                    });
+                } else {    //Creation
+                    openmrsRest.create($scope.resource + "/equipment", query).then(function (response) {
+                        //$scope.equipment = response;
+                        //$scope.getData();
+                        toastr.success($translate.instant('Data saved successfully.'), 'Success');
+                        $state.go('home.equipments');
+                        $scope.loading = false;
+                    }, function (e) {
+                        $scope.loading = false;
+                        console.log(e)
+                        toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+                    });
+                }
+            } else {
+                $scope.loading = false;
+                if (!$scope.equipment.serialNumber || !$scope.equipment.designation) {
+                    toastr.error($translate.instant('You must fill in the required fields before you proceed.'), $translate.instant('Error'));
+                }
+                if ($scope.equipment.serialNumber && ($scope.equipment.serialNumber.length <= 3 || $scope.equipment.serialNumber.length > 120)) {
+                    toastr.error($translate.instant('The equipment serialNumber must be between 3 and 120 characters max.'), $translate.instant('Error'));
+                } else if ($scope.equipment.designation && $scope.equipment.designation == "") {
+                    toastr.error($translate.instant('The equipment designation must not be empty.'), $translate.instant('Error'));
+                }
+            }
+
         }
 
-        loadEquipmnts();
 
-        $scope.view = function (id) {
-
-        }
 
     }]);
