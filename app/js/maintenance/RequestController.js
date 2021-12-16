ec65515cf35db299e642a3dde2c006bae77f45d8
Anatole ABE <anatoleabe@gmail.com>
2021-12-16 12:37:39 +0100

    https://savics.atlassian.net/browse/MRSN-411

--- app/js/maintenance/RequestController.js
+++ app/js/maintenance/RequestController.js
@@ -1,12 +1,81 @@
-angular.module('RequestController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('RequestController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) { 
-    $scope.rootscope = $rootScope;
+angular.module('RequestController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('RequestController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) {
+        $scope.rootscope = $rootScope;
 
-    $scope.maintenances = [];
-    $scope.appTitle = "Request an intervention";
-    $scope.resource = "savicsgmao";
-    //Breadcrumbs properties
-    $rootScope.links = {};
-    $rootScope.links["Home"] = "";
-    $rootScope.links["Maintenance"] = "/maintenance";
-    
-}]);
+        $scope.maintenances = [];
+        $scope.appTitle = "Request an intervention";
+        $scope.resource = "savicsgmao";
+        //Breadcrumbs properties
+        $rootScope.links = {};
+        $rootScope.links["Home"] = "";
+        $rootScope.links["Maintenance"] = "/maintenance";
+        $scope.equipmentSearchText = "";
+        $scope.request = {};
+
+        $scope.searchEquipments = function (searchText) {
+            console.log(searchText)
+            return openmrsRest.getFull($scope.resource + "/equipment?name=" + searchText).then(function (response) {
+                console.log(response);
+                return response.results.filter(function (item) {
+                    return item.name.toLowerCase().includes(searchText.toLowerCase())
+                });
+            }, function (e) {
+                return [];
+            });
+        };
+
+        $scope.selectedEquipmentChange = function (item) {
+            $scope.request.equipment = item;
+        };
+
+
+        $scope.LoadRequest = function () {
+            $scope.loading = true;
+            if ($stateParams.uuid && $stateParams.uuid != "new") {//edit
+                openmrsRest.getFull($scope.resource + "/maintenanceRequest/" + $stateParams.uuid).then(function (response) {
+                    $scope.request = response;
+                    $scope.request.creation = new Date($scope.request.creation);
+                    $scope.selectedEquipment = $scope.request.equipment;
+                }, function (e) {
+                    $scope.loading = false;
+                    toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
+                });
+            } else {
+                $scope.isEditable = true;
+                
+                $scope.loading = false;
+            }
+        }
+
+        $scope.LoadRequest();
+
+
+        $scope.save = function () {
+            if (($scope.request.equipment)) {
+                $scope.loading = true;
+                if ($scope.request && $scope.request.uuid) {//edit
+                    openmrsRest.update($scope.resource + "/maintenanceRequest", $scope.request).then(function (response) {
+                        $scope.request = response;
+                        loadRequest();
+                        toastr.success($translate.instant('The maintenance request has been successfully updated.'), 'Success');
+                    }, function (e) {
+                        console.error(e);
+                        $scope.loading = false;
+                        toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
+                    });
+                } else {//Creation
+                    openmrsRest.create($scope.resource + "/maintenanceRequest", $scope.request).then(function (response) {
+                        $scope.clear();
+                        loadSites();
+                        toastr.success($translate.instant('The maintenance request has been successfully created.'), 'Success');
+                    }, function (e) {
+                        console.error(e);
+                        $scope.loading = false;
+                        toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
+                    });
+                }
+            } else {
+                toastr.error($translate.instant('All required fields must be filled out.'), 'Error');
+            }
+        }
+
+    }]);
