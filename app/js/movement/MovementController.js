f7a63cc56f116ac0932c192c1213e05ea7e7ff7a
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2022-01-04 13:37:55 +0200

    saving somework for MRSN-416

--- app/js/movement/MovementController.js
+++ app/js/movement/MovementController.js
@@ -1,3 +1,24 @@
-angular.module('MovementController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('MovementController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) {    
-    //TODO
+angular.module('MovementController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.data.table']).controller('MovementController', ['$scope', '$rootScope', '$mdToast', '$state', '$stateParams', '$mdDialog', 'openmrsRest', 'toastr', '$translate', function ($scope, $rootScope, $mdToast, $state, $stateParams, $mdDialog, openmrsRest, toastr, $translate) {
+    $scope.lines = [];
+
+
+    $scope.addSendingLine = function () {
+        $scope.lines.push({
+            operation: { equipment_serial_number: "", equipment_id_number: "" }
+        });
+    }
+
+    $scope.deleteSendingDetail = function (sendingDetail, index) {
+        var confirm = $mdDialog.confirm()
+            .title('Confirmation')
+            .textContent($translate.instant('Do you really want to delete this line ?'))
+            .ok('Yes')
+            .cancel('Cancel');
+        $mdDialog.show(confirm).then(function () {
+            $scope.lines.splice(index, 1);
+        }, function () {
+
+        });
+    }
+
 }]);
