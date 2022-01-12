f46f3294ccc267bcf14a35788942ba562bf8d562
Mohamed Kabir Koné <mkabir.kone@gmail.com>
2022-01-12 10:38:58 +0000

    https://savics.atlassian.net/browse/MRSN-408

--- app/js/maintenance/MaintenanceController.js
+++ app/js/maintenance/MaintenanceController.js
@@ -3,31 +3,26 @@ angular.module('MaintenanceController', ['ngMaterial', 'ngAnimate', 'toastr', 'm
     $scope.resource = "savicsgmao";
     $scope.loading = false;
     $rootScope.links = {"GMAO Module": "", "Maintenance Management": "maintenancemanagement"};
-    $scope.maintenance = {};
+    $scope.maintenance = { maintenanceType: {} };
     $scope.equipment = {};
     $scope.item = null;
     $scope.searchText = "";
+    $scope.maintenance_types = [];
 
     $scope.getData = function () {
         $scope.loading = true;
         openmrsRest.getFull($scope.resource + "/maintenanceType").then(function (response) {
             $scope.maintenance_types = response.results;
-            openmrsRest.getFull($scope.resource + "/equipment").then(function (response) {
-                $scope.equipments = response.results;
-                if($stateParams.uuid){
-                    openmrsRest.getFull($scope.resource + "/maintenance/" + $stateParams.uuid).then(function (response) {
-                        $scope.loading = false;
-                        $scope.maintenance = response;
-                        $scope.searchText = $scope.maintenance.equipment.name;                      
-                    },function(e){
-                        $scope.loading = false;
-                        toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
-                    });
-                } else $scope.loading = false;
-            },function(e){
-                $scope.loading = false;
-                toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
-            });
+            if($stateParams.uuid){
+                openmrsRest.getFull($scope.resource + "/maintenance/" + $stateParams.uuid).then(function (response) {
+                    $scope.loading = false;
+                    $scope.maintenance = response;
+                    $scope.searchText = $scope.maintenance.equipment.name;                      
+                },function(e){
+                    $scope.loading = false;
+                    toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+                });
+            } else $scope.loading = false;
         },function(e){
             $scope.loading = false;
             toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
@@ -37,44 +32,32 @@ angular.module('MaintenanceController', ['ngMaterial', 'ngAnimate', 'toastr', 'm
     $scope.save = function(){
         $scope.loading = true;
         var query = JSON.parse(JSON.stringify($scope.maintenance));
-        query.unit = query.unit.id;
-        query.route = query.route.id;
-        openmrsRest.getFull($scope.resource + "/item?code=" + $scope.drug.code).then(function (response) {
-            if (response.results.length == 0 || (response.results[0].uuid == $scope.drug.uuid)) {                
-                if ($scope.drug && $scope.drug.uuid) {    //Edit
-                    openmrsRest.update($scope.resource + "/item", query).then(function (response) {
-                        $scope.drug = response;
-                        $scope.getData();
-                        $state.go('home.drugs', { });  
-                    },function(e){
-                        $scope.loading = false;
-                        toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
-                    });
-                } else {    //Creation
-                    openmrsRest.create($scope.resource + "/item", query).then(function (response) {
-                        $scope.drug = response;
-                        $scope.getData();
-                        $state.go('home.drugs', { });  
-                    },function(e){
-                        $scope.loading = false;
-                        toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
-                    });
-                }
-            } else {
-                $mdDialog.show($mdDialog.alert()
-                .parent(angular.element(document.querySelector('body')))
-                .clickOutsideToClose(true)
-                .textContent($translate.instant('Attention, the drug code you entered is already used. Please use another one.'))
-                .ok('Ok')).then(function(){
-                    $scope.getData();
-                    $scope.loading = false;
-                });
-            }
-        });
+        query.maintenanceType = parseInt($scope.maintenance.maintenanceType.id);
+        query.equipment = parseInt($scope.equipment.id);    
+        query.status = parseInt($scope.status);      
+        if ($scope.maintenance && $scope.maintenance.uuid) {    //Edit
+            openmrsRest.update($scope.resource + "/maintenance", query).then(function (response) {
+                $scope.maintenance = response;
+                $scope.getData();
+                $state.go('home.maintenancemanagement', { });  
+            },function(e){
+                $scope.loading = false;
+                toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+            });
+        } else {    //Creation
+            openmrsRest.create($scope.resource + "/maintenance", query).then(function (response) {
+                $scope.maintenance = response;
+                $scope.getData();
+                $state.go('home.maintenancemanagement', { });  
+            },function(e){
+                $scope.loading = false;
+                toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+            });
+        }
     }
 
     $scope.getEquipments = function (searchText) {
-        return openmrsRest.getFull("equipment?name=" + searchText).then(function (response) {
+        return openmrsRest.getFull($scope.resource + "/equipment?name=" + searchText).then(function (response) {
             return response.results;
         });
     };
