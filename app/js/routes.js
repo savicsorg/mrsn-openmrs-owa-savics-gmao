ce56add12f09fe3f9e6e5f316d65ce0cec4ba6e7
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2022-01-27 14:34:46 +0200

    after finishup with https://savics.atlassian.net/browse/MRSN-456

--- app/js/routes.js
+++ app/js/routes.js
@@ -190,6 +190,7 @@ angular.module('routes', []).config(['$stateProvider', '$urlRouterProvider', '$h
         url: 'movement',
         params: {
             operation_id: undefined,
+            equipment_id: undefined,
             data: undefined
         },
         template: require('./movement/movement.html'),
