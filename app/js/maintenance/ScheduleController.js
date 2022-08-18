e8a7df453e00bde8f04d1342e58d7c33300c851d
Murindangabo cyuzuzo p.damien <zudanga@gmail.com>
2022-08-18 09:09:20 +0200

    https://savics.atlassian.net/browse/MRSN-568

--- app/js/maintenance/ScheduleController.js
+++ app/js/maintenance/ScheduleController.js
@@ -4,178 +4,40 @@ angular.module('ScheduleController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.d
     $scope.rootScope = $rootScope;
     $scope.appTitle = $translate.instant("Maintenance Schedule");
     $scope.resource = "savicsgmao";
-    $rootScope.links = { "Module GMAO": "", "Schedule": "Schedule", "Edit": "Schedule" };
+    $rootScope.links = { "Module GMAO": "", "Schedule": "Schedule", "add": "Schedule" };
     $scope.loading = false;
-    $scope.equipment = {};
+    $scope.maintenance_types = [];
     //Breadcrumbs properties
     $scope.equipement = {};
 
     var dictionary = require("../utils/dictionary");
 
     var schedule_typesjson = require('../../json/maintenance/scheduletype.json');
-    var acquisitionModesjson = require('../../json/equipment/acquisitionModes.json');
-    var disposableManualsjson = require('../../json/equipment/disposableManuals.json');
-    var equipmentServicesjson = require('../../json/equipment/equipmentStatus.json');
     $scope.schedule_types = dictionary.getJsonList(schedule_typesjson, $rootScope.selectedLanguage);
-    $scope.acquisitionModes = dictionary.getJsonList(acquisitionModesjson, $rootScope.selectedLanguage);
-    $scope.disposableManuals = dictionary.getJsonList(disposableManualsjson, $rootScope.selectedLanguage);
-    $scope.equipmentServices = dictionary.getJsonList(equipmentServicesjson, $rootScope.selectedLanguage);
 
-    $scope.allRegions = [];
-    var allDistricts = [];
-    $scope.districts = [];
-    var allHealthCenters = [];
-    $scope.healthCenters = [];
-    var allServices = [];
-    $scope.services = [];
-    var allSites = [];
-    $scope.sites = [];
-    $scope.site = { district: {} };
-
-
-    $scope.regionChanged = function (id) {
-        $scope.equipment.district = undefined;
-        $scope.equipment.healthcenter = undefined;
-        $scope.equipment.service = undefined;
-        $scope.equipment.site = undefined;
-        $scope.healthCenters = [];
-        $scope.services = [];
-        $scope.districts = [];
-        $scope.sites = [];
-        $scope.districts = _.filter(allDistricts, function (item) {
-            return item.regionid === id;
-        });
+    $scope.selectedEquipmentChange = function (item) {
+        if (item) {
+            $scope.equipment = item;
+        }
     }
 
-    $scope.districtChanged = function (id) {
-        $scope.equipment.healthcenter = undefined;
-        $scope.equipment.service = undefined;
-        $scope.equipment.site = undefined;
-        $scope.services = [];
-        $scope.healthCenters = [];
-        $scope.sites = [];
-        $scope.healthCenters = _.filter(allHealthCenters, function (item) {
-            return item.district.id === id;
-        });
-    }
-
-    $scope.healthCenterChanged = function (id) {
-        $scope.equipment.service = undefined;
-        $scope.equipment.site = undefined;
-        $scope.services = [];
-        $scope.sites = [];
-        $scope.services = _.filter(allServices, function (item) {
-            return item.healthcenter.id === id;
-        });
-    }
-
-    $scope.departementChanged = function (id) {
-        $scope.equipment.site = undefined;
-        $scope.sites = [];
-        $scope.sites = _.filter(allSites, function (item) {
-            return item.service.id === id;
-        });
-    }
-
-    function loadAllOpenMRSRegions() {
-        $scope.loading = true;
-        openmrsRest.getFull($scope.resource + "/addressHierarchy?level=2").then(function (response) {
-            $scope.loading = false;
-            $scope.allRegions = response.results;
-        }, function (e) {
-            $scope.loading = false;
-            console.error(e);
-            toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
-        });
-    }
-
-    function loadAllDistricts() {
-        $scope.loading = true;
-        openmrsRest.getFull($scope.resource + "/district").then(function (response) {
-            $scope.loading = false;
-            allDistricts = response.results;
-            if ($stateParams.equipment_id) {
-                $scope.districts = response.results;
-            }
-        }, function (e) {
-            $scope.loading = false;
-            console.error(e);
-            toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
-        });
-    }
-
-    function loadAllHealthCenters() {
-        $scope.loading = true;
-        openmrsRest.getFull($scope.resource + "/healthcenter").then(function (response) {
-            allHealthCenters = response.results;
-            $scope.loading = false;
-            if ($stateParams.equipment_id) {
-                $scope.healthCenters = response.results;
-            }
-        }, function (e) {
-            console.error(e);
-            $scope.loading = false;
-            toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
-        });
-    }
-
-    function loadAllServices() {
-        $scope.loading = true;
-        openmrsRest.getFull($scope.resource + "/service").then(function (response) {
-            allServices = response.results;
-            $scope.loading = false;
-            if ($stateParams.equipment_id) {
-                $scope.services = response.results;
-            }
-        }, function (e) {
-            console.error(e);
-            $scope.loading = false;
-            toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
-        });
-    }
-
-    function loadAllSites() {
-        $scope.loading = true;
-        openmrsRest.getFull($scope.resource + "/site").then(function (response) {
-            $scope.loading = false;
-            allSites = response.results;
-            if ($stateParams.equipment_id) {
-                $scope.sites = response.results;
-            }
-        }, function (e) {
-            $scope.loading = false;
-            console.error(e);
-            toastr.error($translate.instant('An unexpected error has occured.'), 'Error');
-        });
-    }
-
-    function loadEquipmentTypes() {
-        return openmrsRest.getFull($scope.resource + "/equipmentType").then(function (response) {
-            $scope.equipmentTypes = response.results;
-        }, function (e) {
-            $scope.equipmentTypes = [];
+    $scope.getEquipments = function (searchText) {
+        return openmrsRest.getFull($scope.resource + "/equipment?name=" + searchText).then(function (response) {
+            return response.results;
         });
     };
 
-    loadAllOpenMRSRegions();
-    loadAllDistricts();
-    loadAllHealthCenters();
-    loadAllServices();
-    loadAllSites();
-    loadEquipmentTypes();
-
-
-    if ($stateParams.equipment_id) {
-        $scope.equipment = $stateParams.data;
-        // $scope.site = $stateParams.data.site;
-        $scope.equipment.equipmentType = $stateParams.data.equipmentType.id;
-        $scope.equipment.acquisitionDate = new Date(moment(new Date($stateParams.data.acquisitionDate)).format('MM/DD/YYYY'));
-        $scope.equipment.country = $stateParams.data.site.service.healthcenter.district.regionid;
-        $scope.equipment.countrySanitary = $stateParams.data.site.service.healthcenter.district.id;
-        $scope.equipment.hdcsi = $stateParams.data.site.service.healthcenter.id;
-        $scope.equipment.departementhdcsi = $stateParams.data.site.service.id;
-        $scope.equipment.site = $stateParams.data.site.id;
+    $scope.getData = function () {
+        $scope.loading = true;
+        openmrsRest.getFull($scope.resource + "/maintenanceType").then(function (response) {
+            $scope.maintenance_types = response.results;
+            $scope.loading = false;
+        }, function (e) {
+            $scope.loading = false;
+            toastr.error($translate.instant('An unexpected error has occured.'), $translate.instant('Error'));
+        });
     }
+
 
     $scope.save = function () {
         $scope.loading = true;
@@ -219,6 +81,6 @@ angular.module('ScheduleController', ['ngMaterial', 'ngAnimate', 'toastr', 'md.d
         }
     }
 
-
+    $scope.getData();
 
 }]);
